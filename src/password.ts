598e8beebc8f571e7be942ab73e82c1b89613445
irohafox <matsukawa@grabss.co.jp>
2024-10-31 14:28:33 +0900

    Feat: password sync functions

--- src/password.ts
+++ src/password.ts
@@ -4,6 +4,17 @@ export function createHashedPassword(plain: string, saltRounds: number = 10) {
   return bcrypt.hash(plain, saltRounds)
 }
 
+export function createHashedPasswordSync(
+  plain: string,
+  saltRounds: number = 10
+) {
+  return bcrypt.hashSync(plain, saltRounds)
+}
+
 export function comparePassword(plain: string, hashed: string) {
   return bcrypt.compare(plain, hashed)
+}
+
+export function comparePasswordSync(plain: string, hashed: string) {
+  return bcrypt.compareSync(plain, hashed)
 }
