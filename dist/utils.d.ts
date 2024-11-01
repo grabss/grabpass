b2fe7c7d3e534768c12786faa6c7b5148065005e
Yusuke Matsukawa <matsukawa@grabss.co.jp>
2024-11-01 15:57:44 +0900

    Feature/prepare (#7)
    * Chore: ignore dist
    
    * Chore: npm-scripts / prepare

--- dist/utils.d.ts
+++
@@ -1,5 +0,0 @@
-import * as crypto from 'crypto';
-export declare function createHmacToken(data: string, algorithm?: string, encoding?: crypto.BinaryToTextEncoding): {
-    token: string;
-    createdAt: Date;
-};
