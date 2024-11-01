a9c8c2c8f1f46da7fb88fadfb558e941e4312ff5
irohafox <matsukawa@grabss.co.jp>
2024-11-01 15:47:16 +0900

    Chore: ignore dist

--- dist/index.d.ts
+++
@@ -1,3 +0,0 @@
-export * from './grabpass';
-export * from './password';
-export * from './utils';
--- dist/password.d.ts
+++
@@ -1,4 +0,0 @@
-export declare function createHashedPassword(plain: string, saltRounds?: number): Promise<string>;
-export declare function createHashedPasswordSync(plain: string, saltRounds?: number): string;
-export declare function comparePassword(plain: string, hashed: string): Promise<boolean>;
-export declare function comparePasswordSync(plain: string, hashed: string): boolean;
